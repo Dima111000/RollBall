8147a2069c1e750eeb6fd264ff7fefb17a6552d4
Dima111000 <74449630+Dima111000@users.noreply.github.com>
2022-02-08 04:16:40 +0200

    Add files via upload

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class Control : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Собрано: " + count.ToString();
+        if (count >= 8)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+    void Update()
+    {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
